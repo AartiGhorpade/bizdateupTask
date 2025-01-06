3b491d1ec9a1cb825919fcbfa4957957cb4babca
AartiGhorpade <89845285+AartiGhorpade@users.noreply.github.com>
2025-01-07 00:22:56 +0530

    storedtask route created at backend

--- Backend/app.js
+++ Backend/app.js
@@ -2,34 +2,43 @@ import express from 'express'
 import cors from 'cors'
 const app = express()
 import dotenv from 'dotenv'
-dotenv.config() 
+dotenv.config()
 
 
-app.use(cors()) 
+app.use(cors())
 app.use(express.json()) // Parse JSON request bodies
 
 let tasks = [] // Because of no db used array used here
-const PORT = process.env.PORT || 3002; 
+const PORT = process.env.PORT || 3002;
 
 // Root route to check if the server is working
 app.get('/', (req, resp) => {
-    resp.send('working') 
+    resp.send('working')
 })
 
 // to store a new task
 app.post('/storeTask', async (req, resp) => {
     try {
-        tasks.push(req.body) 
+        tasks.push(req.body)
         resp.status(201).send({ message: 'Task Added Successfully', tasks }) // Send success response
     } catch (e) {
-        resp.status(500).send({ message: 'Failed to add tasks' }); 
+        resp.status(500).send({ message: 'Failed to add tasks' });
+    }
+})
+
+// to store a new task
+app.get('/storedTasks', async (req, resp) => {
+    try {
+        resp.send(tasks)
+    } catch (e) {
+        resp.status(500).send({ message: 'Failed to add tasks' });
     }
 })
 
 //  to update the tasks 
 app.post('/updateTasks', async (req, resp) => {
     try {
-        tasks = req.body; 
+        tasks = req.body;
         resp.status(200).send({ message: 'Task updated successfully', tasks });
     } catch (e) {
         resp.status(500).send({ message: 'Failed to update tasks' });
@@ -39,15 +48,15 @@ app.post('/updateTasks', async (req, resp) => {
 //  to delete a specific task
 app.delete('/deleteTasks', async (req, resp) => {
     try {
-        const { taskIndex } = req.body; 
+        const { taskIndex } = req.body;
         tasks.splice(taskIndex, 1); // Remove the task from the tasks array
-        resp.status(200).send({ message: 'Task deleted successfully', tasks }); 
+        resp.status(200).send({ message: 'Task deleted successfully', tasks });
     } catch (e) {
-        resp.status(500).send({ message: 'Failed to delete task' }); 
+        resp.status(500).send({ message: 'Failed to delete task' });
     }
 });
 
 // Start the server 
 app.listen(PORT, () => {
-    console.log(`Running on ${PORT}`); 
+    console.log(`Running on ${PORT}`);
 })
